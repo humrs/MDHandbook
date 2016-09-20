8cfa6fbf54cbd0cd8f2d617bee4ee41121c1f1f4
Stanley Hum <r.stanley.hum@gmail.com>
2016-09-20 11:45:01 -0400

    added MenuPage

--- src/MDHandbookApp.Forms/AppBootstrapper.cs
+++ src/MDHandbookApp.Forms/AppBootstrapper.cs
@@ -14,13 +14,14 @@ namespace MDHandbookApp.Forms
     {
         public void OnInitializedNavigation(INavigationService _nav)
         {
-            _nav.NavigateAsync("NavPage/MainPage", animated: false);
+            _nav.NavigateAsync("/MenuPage/NavPage/MainPage", animated: false);
         }
 
         public void RegisterTypes(IUnityContainer _container)
         {
             _container.RegisterTypeForNavigation<MainPage>();
             _container.RegisterTypeForNavigation<NavPage>();
+            _container.RegisterTypeForNavigation<MenuPage>();
         }
 
         public void InitializeMDHandbookServices(IUnityContainer _container)
